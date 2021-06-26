e055e70b27329850e972cb0be68341bc9fe13741
kaedh <leonardomds2013@gmail.com>
2021-06-26 12:02:24 -0300

    :sparkles: finish Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
+import '../styles/Card.css';
 import avatar from '../assets/avatar1.svg';
 
-function Card() {
+function Card({
+  firstName, lastName, isSelected, onClick,
+}) {
+  const selected = isSelected ? 'selected' : '';
+
   return (
-    <button type="button">
-      <img src={avatar} alt="" />
+    <button className={`customer-btn ${selected}`} type="button" onClick={onClick}>
+      <img className="customer-avatar" src={avatar} alt="" />
 
       <div className="text-wrapper">
-        <span>Leonardo</span>
-        <span>Martins de Sousa</span>
+        <span>{firstName}</span>
+        <span>{lastName}</span>
       </div>
     </button>
   );
 }
+
+Card.propTypes = {
+  firstName: PropTypes.string.isRequired,
+  lastName: PropTypes.string.isRequired,
+  isSelected: PropTypes.bool,
+  onClick: PropTypes.func.isRequired,
+};
+
+Card.defaultProps = {
+  isSelected: false,
+};
 
 export default Card;
