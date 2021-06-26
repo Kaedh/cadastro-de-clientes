eef28e99a6d199e6377b6fe08e50514e10d07534
kaedh <leonardomds2013@gmail.com>
2021-06-26 11:21:19 -0300

    :sparkles: import card component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import Card from './Card';
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div>
+        <Card />
+      </div>
     </div>
   );
 }
