59496383833cafcfe5b535d2c63e70412ade80b4
kaedh <leonardomds2013@gmail.com>
2021-07-10 10:14:50 -0300

    :package: add react-input-mask

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
-import Card from './Card';
+import Input from './Input';
 
 function App() {
+  const {
+    register, handleSubmit, formState: { errors },
+  } = useForm();
+
+  const inputValidations = {
+    firstName: {
+      required: { value: true, message: 'Por favor insira um nome' },
+    },
+  };
+
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div className="App">
-      <div>
-        <Card />
-      </div>
+      <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
+        <Input
+          mask="999.999.999-99"
+          width={615}
+          placeholder="Nome"
+          register={() => register('firstName', inputValidations.firstName)}
+          error={errors.firstName?.message}
+        />
+        <input type="submit" />
+      </form>
     </div>
   );
 }
